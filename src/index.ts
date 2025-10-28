83845c330153ed63f60a057757971bee6197826b
linrongbin16 <linrongbin16@outlook.com>
2025-10-28 15:54:02 +0800

    c

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import { quit } from "./quit.js";
-import { write } from "./write.js";
+import quit from "./quit.js";
+import write from "./write.js";
+
+export type CommandAttributes = {
+  bang: boolean;
+  nargs: "0" | "1" | "*" | "+" | "?";
+};
+
+export type CommandOptions = {
+  force: boolean;
+  alias?: string;
+};
+
+export type CommandDefinition = {
+  name: string;
+  callback: (ctx: any) => Promise<void>;
+  attributes: CommandAttributes;
+  options: CommandOptions;
+};
 
 type SetupOptions = {
   force: boolean;
@@ -13,12 +30,17 @@ function setDefaultSetupOptions(options: object, defaultOptions: SetupOptions) {
   }
 }
 
-const ex = {
-  setup(options?: SetupOptions): void {
-    options = options ?? { force: true };
-    setDefaultSetupOptions(options, { force: true });
+function setup(setupOptions?: SetupOptions): void {
+  setupOptions = setupOptions ?? { force: true };
+  setDefaultSetupOptions(setupOptions, { force: true });
 
-    Rsvim.cmd.create("quit", quit, undefined, { alias: "q" });
-    Rsvim.cmd.create("write", write, undefined, { alias: "w" });
-  },
-};
+  [quit, write].forEach((cmd: CommandDefinition) => {
+    let options = cmd.options;
+    if (typeof setupOptions.force === "boolean") {
+      options.force = setupOptions.force;
+    }
+    Rsvim.cmd.create(cmd.name, cmd.callback, cmd.attributes, options);
+  });
+}
+
+export default { setup };
