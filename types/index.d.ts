697dfac1ce93ea84619e47c33f7cd7e03632937d
linrongbin16 <linrongbin16@outlook.com>
2025-10-28 15:54:09 +0800

    c

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,22 @@
-export {};
+export type CommandAttributes = {
+    bang: boolean;
+    nargs: "0" | "1" | "*" | "+" | "?";
+};
+export type CommandOptions = {
+    force: boolean;
+    alias?: string;
+};
+export type CommandDefinition = {
+    name: string;
+    callback: (ctx: any) => Promise<void>;
+    attributes: CommandAttributes;
+    options: CommandOptions;
+};
+type SetupOptions = {
+    force: boolean;
+};
+declare function setup(setupOptions?: SetupOptions): void;
+declare const _default: {
+    setup: typeof setup;
+};
+export default _default;
