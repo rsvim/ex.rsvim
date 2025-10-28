d26f11f4f1f43e18c15b76e202135e51e2b74e20
linrongbin16 <linrongbin16@outlook.com>
2025-10-28 15:26:09 +0800

    c

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { write } from "./write.ts";
 const ex = {
   setup(): void {
     Rsvim.cmd.create("quit", quit, undefined, { alias: "q" });
+    Rsvim.cmd.create("write", write, undefined, { alias: "w" });
   },
 };
