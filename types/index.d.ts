eae18adae7bc17aaf12eb6c7b5b6f446fa853bc1
linrongbin16 <linrongbin16@outlook.com>
2025-10-31 16:02:46 +0800

    c

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,16 @@
-export type CommandAttributes = {
+export type ExAttributes = {
     bang: boolean;
     nargs: "0" | "1" | "*" | "+" | "?";
 };
-export type CommandOptions = {
+export type ExOptions = {
     force: boolean;
     alias?: string;
 };
-export type CommandDefinition = {
+export type ExDefinition = {
     name: string;
     callback: (ctx: any) => Promise<void>;
-    attributes: CommandAttributes;
-    options: CommandOptions;
+    attributes: ExAttributes;
+    options: ExOptions;
 };
 export type SetupOptions = {
     force: boolean;
