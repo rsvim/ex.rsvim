6c62033fcd84ad8c22930767776d2150ca4b4345
linrongbin16 <linrongbin16@outlook.com>
2026-04-23 16:36:52 +0800

    chore

--- sync.py
+++ sync.py
@@ -8,18 +8,18 @@ from pathlib import Path
 
 CWD = Path(os.getcwd())
 RSVIM_TYPES = CWD.parent / "rsvim" / "types"
-EX_TYPES = CWD / "vendor" / "@rsvim" / "types"
+PLUGIN_TYPES = CWD / "vendor" / "@rsvim" / "types"
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     logging.info(f"CWD:{CWD}")
     logging.info(f"RSVIM_TYPES:{RSVIM_TYPES}")
-    logging.info(f"EX_TYPES:{EX_TYPES}")
+    logging.info(f"PLUGIN_TYPES:{PLUGIN_TYPES}")
 
-    command = f"rm -rf {EX_TYPES}"
+    command = f"rm -rf {PLUGIN_TYPES}"
     logging.info(f"Run: {command}")
     os.system(command)
-    command = f"cp -rf {RSVIM_TYPES} {EX_TYPES}"
+    command = f"cp -rf {RSVIM_TYPES} {PLUGIN_TYPES}"
     logging.info(f"Run: {command}")
     os.system(command)
